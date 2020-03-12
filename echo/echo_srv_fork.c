b3575613e5b7c1756f2ae4444b15a99eafdea113
HenryLion <anbreakdy@outlook.com>
2020-03-12 23:35:53 +0800

    add signal dispos

--- echo/echo_srv_fork.c
+++ echo/echo_srv_fork.c
@@ -43,6 +43,25 @@ again:
 	}
 }
 
+// 关注一下waitpid的返回值,根据这三个返回值就可以解释为何下面的sig_chld函数可以等待所有的子进程结束.
+
+// > 0: the process ID of the child whose state has changed
+// = 0: if WNOHANG was specified and one or more child(ren) specified by pid exist, but have not yet changed state
+// < 0: error
+
+// 假设一个父进程fork了5个子进程:
+// 其中一个子进程结束,则此子进程给父进程发送SIGCHLD信号，父进程调用sig_chld处理函数,waitpid 返回>0,再次循环调用waitpid因为此时其他进程无状态变化所有返回0,sig_chld函数退出。
+// 又有3个进程同时退出,假设由于linux的信号递送机制，只有1个信号递送给父进程,父进程调用sig_chld处理函数,waitpid 返回>0,再次循环调用waitpid处理完其他两个退出的进程后返回0,sig_chld退出
+
+
+void sig_chld (int sig)
+{
+	pid_t pid;
+	int stat;
+	while ( (pid = waitpid (-1,&stat,WNOHANG)) > 0)
+		printf ("child %d terminated\n", pid);
+	return;
+}
 
 int main (void)
 {
@@ -65,10 +84,21 @@ int main (void)
 
 	Listen (listen_fd, 6);
 
+	// 添加SIGCHID信号的处理
+	signal (SIGCHLD, sig_chld);
+
 	while (1)
 	{
 		ad_len = sizeof (cli_addr);
-		conn_fd = Accept (listen_fd, (struct sockaddr *)&cli_addr, &ad_len);
+		if ( ( conn_fd = accept (listen_fd, (struct sockaddr *)&cli_addr, &ad_len) ) < 0)
+		{
+			//为了防止父进程在accept时被信号打断(如SIGCHID信号),需要判断错误码，如果是被信号中断,则重新accept
+			if (errno == EINTR)
+				continue;
+			else
+				err_sys ("conect error");
+		}
+		
 
 		if ( (pid = fork()) == 0) // 子进程处理
 		{
